86b91629513e8427de236c7f606e4688911b619e
skillupmk <64582760+skillupmk@users.noreply.github.com>
2020-05-27 12:59:17 +0200

    just test

---
+++ script.js
@@ -0,0 +1,23 @@
+// var slider = tns({
+//     container: '.my-slider',
+//     items: 1,
+//     slideBy: 1,
+//     gutter: 12,
+//     nav: false,
+//     loop: false,
+//     autoplay: false,
+// 		loop: true,
+// 		rewind: true,
+//   });
+
+  var slider = tns({
+    container: '.my-slider',
+    items: 3,
+    mouseDrag: true,
+    slideBy: 'page',
+    swipeAngle: false,
+    speed: 400,
+    nav: false,
+    gutter: 10,
+  });
+  
